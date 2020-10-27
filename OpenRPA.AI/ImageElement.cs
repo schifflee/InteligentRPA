b0289943ae9bdf0a5518eeaaaf79792275a558e2
sunkang <632936617@qq.com>
2020-10-27 17:37:51 +0800

    图片转文本

---
+++ OpenRPA.AI/ImageElement.cs
@@ -0,0 +1,192 @@
+﻿using OpenRPA.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA.AI
+{
+    public class ImageElement : IElement, IDisposable
+    {
+        public string Name { get; set; }
+        public string Processname { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+        public float Confidence { get; set; }
+        private System.Drawing.Bitmap _element = null;
+        public System.Drawing.Bitmap element {
+            get
+            {
+                if(_element==null)
+                {
+                    _element = Interfaces.Image.Util.Screenshot(Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+                }
+                return _element;
+            }
+            set
+            {
+                _element = value;
+            }
+        }
+        object IElement.RawElement { get => element; set => element = value as System.Drawing.Bitmap; }
+        public System.Drawing.Rectangle Rectangle
+        {
+            get
+            {
+                return new System.Drawing.Rectangle(X, Y, Width, Height);
+            }
+            set
+            {
+                X = value.X;
+                Y = value.Y;
+                Width = value.Width;
+                Height = value.Height;
+            }
+        }
+        public ImageElement(System.Drawing.Rectangle Rectangle)
+        {
+            X = Rectangle.X;
+            Y = Rectangle.Y;
+            Width = Rectangle.Width;
+            Height = Rectangle.Height;
+        }
+        public ImageElement(System.Drawing.Rectangle Rectangle, System.Drawing.Bitmap Element)
+        {
+            X = Rectangle.X;
+            Y = Rectangle.Y;
+            Width = Rectangle.Width;
+            Height = Rectangle.Height;
+            this.element = Element;
+        }
+        public void Click(bool VirtualClick, Input.MouseButton Button, int OffsetX, int OffsetY, bool DoubleClick, bool AnimateMouse)
+        {
+            if (AnimateMouse)
+            {
+                FlaUI.Core.Input.Mouse.MoveTo(new System.Drawing.Point(Rectangle.X + OffsetX, Rectangle.Y + OffsetY));
+            } else
+            {
+                NativeMethods.SetCursorPos(Rectangle.X + OffsetX, Rectangle.Y + OffsetY);
+            }
+            Input.InputDriver.Click(Button);
+            if (DoubleClick) Input.InputDriver.Click(Button);
+        }
+        public void Focus()
+        {
+            throw new NotImplementedException();
+        }
+        public Task Highlight(bool Blocking, System.Drawing.Color Color, TimeSpan Duration)
+        {
+            if (!Blocking)
+            {
+                Task.Run(() => _Highlight(Color, Duration));
+                return Task.CompletedTask;
+            }
+            return _Highlight(Color, Duration);
+        }
+        public Task _Highlight(System.Drawing.Color Color, TimeSpan Duration)
+        {
+            using (Interfaces.Overlay.OverlayWindow _overlayWindow = new Interfaces.Overlay.OverlayWindow(true))
+            {
+                _overlayWindow.BackColor = Color;
+                _overlayWindow.Visible = true;
+                _overlayWindow.SetTimeout(Duration);
+                _overlayWindow.Bounds = Rectangle;
+                var sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+                do
+                {
+                    System.Threading.Thread.Sleep(10);
+                    _overlayWindow.TopMost = true;
+                } while (_overlayWindow.Visible && sw.Elapsed < Duration);
+                return Task.CompletedTask;
+            }
+        }
+        public string ImageString()
+        {
+            if (element == null)
+            {
+                var AddedWidth = 10;
+                var AddedHeight = 10;
+                var ScreenImageWidth = Rectangle.Width + AddedWidth;
+                var ScreenImageHeight = Rectangle.Height + AddedHeight;
+                var ScreenImagex = Rectangle.X - (AddedWidth / 2);
+                var ScreenImagey = Rectangle.Y - (AddedHeight / 2);
+                if (ScreenImagex < 0) ScreenImagex = 0; if (ScreenImagey < 0) ScreenImagey = 0;
+                using (var image = Interfaces.Image.Util.Screenshot(ScreenImagex, ScreenImagey, ScreenImageWidth, ScreenImageHeight, Interfaces.Image.Util.ActivityPreviewImageWidth, Interfaces.Image.Util.ActivityPreviewImageHeight))
+                {
+                    return Interfaces.Image.Util.Bitmap2Base64(image);
+                }
+            }
+            else
+            {
+                return Interfaces.Image.Util.Bitmap2Base64(element);
+            }
+        }
+        public void Dispose()
+        {
+            if(_element!=null) _element.Dispose();
+        }
+        private Emgu.CV.OCR.Tesseract _ocr;
+        public string Value
+        {
+            get
+            {
+                try
+                {
+                    if(!string.IsNullOrEmpty(Text))
+                    {
+                        return Text;
+                    }
+                    var lang = Config.local.ocrlanguage;
+                    string basepath = Interfaces.Extensions.DataDirectory;
+                    string path = System.IO.Path.Combine(basepath, "tessdata");
+                    ocr.TesseractDownloadLangFile(path, Config.local.ocrlanguage);
+                    ocr.TesseractDownloadLangFile(path, "osd");
+                    _ocr = new Emgu.CV.OCR.Tesseract(path, lang.ToString(), Emgu.CV.OCR.OcrEngineMode.TesseractLstmCombined);
+                    _ocr.Init(path, lang.ToString(), Emgu.CV.OCR.OcrEngineMode.TesseractLstmCombined);
+                    _ocr.PageSegMode = Emgu.CV.OCR.PageSegMode.SparseText;
+
+                    // OpenRPA.Interfaces.Image.Util.SaveImageStamped(element, "OCR");
+                    using (var img = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>(element))
+                    {
+                        return ocr.OcrImage(_ocr, img.Mat);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.ToString());
+                    return null;
+                }
+            }
+            set
+            {
+                Text = value;
+            }
+        }
+        public IElement[] Items
+        {
+            get
+            {
+                return new IElement[] { };
+            }
+        }
+        public bool SaveAs(string filename)
+        {
+            try
+            {
+                element.Save(filename);
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                Log.Error(ex.ToString());
+            }
+            return false;
+        }
+
+    }
+}
---
+++ OpenRPA.AI/ImageEvent.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA.AI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    class ImageEvent
+    {
+        private ImageEvent() { }
+        private Bitmap template;
+        private System.Threading.AutoResetEvent waitHandle;
+        private System.Timers.Timer timer;
+        private Rectangle[] results = new Rectangle[] { };
+        private string Processname = null;
+        private bool CompareGray = false;
+        private bool running = false;
+        private Double threshold;
+        private Rectangle limit;
+        public static Rectangle[] waitFor(Bitmap Image, Double Threshold, String Processname, TimeSpan TimeOut, bool CompareGray, Rectangle Limit)
+        {
+
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            var me = new ImageEvent();
+            me.limit = Limit;
+            me.Processname = Processname;
+            // rpaactivities.image.util.saveImage(Image, "waitFor-Image");
+            me.template = Image;
+
+            me.CompareGray = CompareGray;
+            me.threshold = Threshold;
+            me.running = true;
+            if (me.findMatch()) return me.results;
+
+            if (TimeOut.TotalMilliseconds < 100) return me.results;
+            me.timer = new System.Timers.Timer();
+            me.timer.Elapsed += new System.Timers.ElapsedEventHandler(me.onElapsed);
+            me.timer.AutoReset = true;
+            me.timer.Interval = 100;
+            me.timer.Start();
+
+            me.waitHandle = new System.Threading.AutoResetEvent(false);
+            me.waitHandle.WaitOne(TimeOut);
+            me.running = false;
+            me.timer.Stop();
+            return me.results;
+        }
+        private void onElapsed(object sender, EventArgs e)
+        {
+            timer.Stop();
+            if (!running) return;
+            try
+            {
+                var with = template.Width;
+            }
+            catch (Exception)
+            {
+                running = false;
+                waitHandle.Set();
+            }
+            if (findMatch())
+            {
+                running = false;
+                waitHandle.Set();
+            }
+            timer.Interval = 100;
+            if (running) timer.Start();
+        }
+        private bool findMatch()
+        {
+            try
+            {
+                //var results = new List<Rectangle>();
+                if (!running) return false;
+                try
+                {
+                    var with = template.Width;
+                }
+                catch (Exception)
+                {
+                    running = false;
+                    //waitHandle.Set();
+                }
+                if (string.IsNullOrEmpty(Processname))
+                {
+                    var desktop = Interfaces.Image.Util.Screenshot();
+                    GC.KeepAlive(template);
+                    var results = Matches.FindMatches(desktop, template, threshold, 10, CompareGray);
+                    if (results.Count() > 0)
+                    {
+                        this.results = results;
+                        return true;
+                    }
+                }
+                else
+                {
+                    var ps = System.Diagnostics.Process.GetProcessesByName(Processname);
+                    foreach (var p in ps)
+                    {
+                        var rects = new List<Rectangle>();
+
+                        var allChildWindows = MyEnumWindows.GetWindows(true, p.Id);
+                        // var allChildWindows = new WindowHandleInfo(p.MainWindowHandle).GetAllChildHandles();
+                        allChildWindows.Add(p.MainWindowHandle);
+                        foreach (var window in allChildWindows)
+                        {
+                            WindowHandleInfo.RECT rct;
+                            if (!WindowHandleInfo.GetWindowRect(new HandleRef(this, window), out rct))
+                            {
+                                continue;
+                            }
+                            var rect = new Rectangle(rct.Left, rct.Top, rct.Right - rct.Left + 1, rct.Bottom - rct.Top + 1);
+                            if (!limit.IsEmpty)
+                            {
+                                rect = new Rectangle(rect.X + limit.X, rect.Y + limit.Y, limit.Width, limit.Height);
+                            }
+                            if (rect.Width < template.Width || rect.Height < template.Height)
+                            {
+                                continue;
+                            }
+                            if((rect.X > 0 || (rect.X + rect.Width) > 0) &&
+                                    (rect.Y > 0 || (rect.Y + rect.Height) > 0)) {
+                                rects.Add(rect);
+                            }
+                        }
+                        foreach (var rect in rects.ToList())
+                        {
+                            foreach (var subrect in rects.ToList())
+                            {
+                                if (rect.Contains(subrect) && !rect.Equals(subrect))
+                                {
+                                    rects.Remove(subrect);
+                                }
+                            }
+
+                        }
+                        foreach (var rect in rects)
+                        {
+                            // System.Diagnostics.Trace.WriteLine("**** Match within window at " + rect.ToString());
+                            var desktop = Interfaces.Image.Util.Screenshot(rect);
+                            try
+                            {
+                                var results = Matches.FindMatches(desktop, template, threshold, 10, CompareGray);
+                                if (results.Count() > 0)
+                                {
+                                    var finalresult = new List<Rectangle>();
+                                    for(var i = 0; i < results.Length; i ++)
+                                    {
+                                        results[i].X += rect.X;
+                                        results[i].Y += rect.Y;
+                                    }
+
+                                    this.results = results;
+                                    //this.result = results[0];
+                                    // this.result = new Rectangle(rect.X + results[0].X, rect.Y + results[0].Y, results[0].Width, results[0].Height);
+
+                                    //if (!limit.IsEmpty)
+                                    //{
+                                    //    this.result = new Rectangle((int)w.BoundingRectangle.X + limit.X + results[0].X, (int)w.BoundingRectangle.Y + limit.Y + results[0].Y, results[0].Width, results[0].Height);
+                                    //}
+                                    return true;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                System.Diagnostics.Trace.WriteLine(ex.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Interfaces.Log.Error(ex.ToString());
+            }
+            return false;
+        }
+    }
+}
